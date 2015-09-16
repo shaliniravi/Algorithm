e3f463446c5769033afe8ff5192a484b0268dc5e
Shalini Ravishankar <shalini.ravishankar@gmail.com>
2015-09-15 20:39:08 -0500

    solutions

---
+++ src/week1/PercolationStats.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class PercolationStats {
+
+	public PercolationStats() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
