a3960317f11a980b6dc3774332022fa576ed73fb
Shalini Ravishankar <shalini.ravishankar@gmail.com>
2015-09-16 00:15:54 -0500

    check style added

--- src/week1/Percolation.java
+++ src/week1/Percolation.java
@@ -4,27 +4,28 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
  * Compilation: javac Percolation.java Execution: API only. Must be called from another class.
- * Dependencies: algs4.jar, WeightedQuickUnionUF.java
- * This program estimates the value of the percolation threshold via Monte Carlo simulation.
- * Author : Shalini
+ * Dependencies: algs4.jar, WeightedQuickUnionUF.java This program estimates the value of the
+ * percolation threshold via Monte Carlo simulation. Author : Shalini
  */
 
 public class Percolation {
 
-  private final int size; //size of the N * N matrix
-  private boolean[] field; // boolean array to block or open the site
+  private final int size; // size of the N * N matrix
+  private final boolean[] field; // boolean array to block or open the site
   WeightedQuickUnionUF quickUnionUF1; // Used to avoid backwash
-  WeightedQuickUnionUF quickUnionUF2; 
-  private final int topRoot; //virtual top root
-  private final int bottomRoot; //virtual bottom root
+  WeightedQuickUnionUF quickUnionUF2;
+  private final int topRoot; // virtual top root
+  private final int bottomRoot; // virtual bottom root
   private final int rowLength;
 
   /**
-   * Initialize the object and creates N-by-N grid, with all sites blocked.
-   * @param gridSize 
+   * Constructor to initialize objects.
+   * 
+   * @param gridSize row size of the given grid
    */
   public Percolation(int gridSize) {
 
+    // base case
     if (gridSize <= 0) {
       throw new IllegalArgumentException("N must be greater than 0");
     }
@@ -40,29 +41,6 @@ public class Percolation {
 
   }
 
-  /**
-   * @param row
-   * @param column
-   * @returns the 1D coordinate value of the given field's 2D grid coordinate.
-   */
-  private int xyTo1D(int row, int  column) {
-
-    row = row - 1;
-    column =  column - 1;
-    return (row * rowLength + column);
-  }
-
-  // Validating integers
-  private void validate(int row, int column) {
-
-    if (row < 1 || row > rowLength || column < 1 || column > rowLength) {
-      throw new java.lang.IndexOutOfBoundsException(
-          "The values must be between 1 and " + rowLength);
-    }
-
-
-  }
-
   // creates N-by-N grid, with all sites blocked
   private void createGrid() {
     for (int i = 0; i < size; i++) {
@@ -71,15 +49,58 @@ public class Percolation {
   }
 
   /**
-   * Opens the given site if it is not already open.
-   * @param row
-   * @param column
+   * Checks if the site is open and also connected to the top virtual row.
    * 
+   * @param row row value in the matrix
+   * @param column column value in the matrix
+   * @return true or false whether the site is full or not
+   */
+  public boolean isFull(int row, int column) {
+
+    // validate the row and column values
+    validate(row, column);
+
+    // Checks if the site is open
+    if (isOpen(row, column)) {
+
+      final int index = xyTo1D(row, column);
+      if (quickUnionUF2.connected(index, topRoot)) {
+
+        return true;
+      }
+      return false;
+    }
+    return false;
+
+  }
+
+  /**
+   * Checks if the site (row i, column j) is opens or not.
+   * 
+   * @param row row value in the matrix
+   * @param column column value in the matrix
+   * @return true or false whether the site is open or not
+   */
+  public boolean isOpen(int row, int column) {
+
+    // validate the row and column values
+    validate(row, column);
+    final int pos = xyTo1D(row, column);
+    return field[pos];
+  }
+
+  //
+  /**
+   * Opens the site (row i, column j) if it is not open already.
+   * 
+   * @param row row value in the matrix
+   * @param column column value in the matrix
    */
   public void open(int row, int column) {
 
+    // validate the row and column values
     validate(row, column);
-    int position = xyTo1D(row, column);
+    final int position = xyTo1D(row, column);
     if (!isOpen(row, column)) {
       field[position] = true;
       unionNeighbors(row, column, position);
@@ -89,9 +110,28 @@ public class Percolation {
   }
 
   /**
-   * @param row
-   * @param column
-   * @param position This Method connects the adjacent fields.
+   * Checks if the virtual top root and bottom root are connected.
+   *
+   * @return true if the system percolates
+   */
+  public boolean percolates() {
+
+    // checks if the virtual top root and bottom root are connected.
+    if (quickUnionUF1.connected(topRoot, bottomRoot)) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+
+  /**
+   * Connects the adjacent sites.
+   * 
+   * @param row row value in the matrix
+   * @param column column value in the matrix
+   * @param position current position of the site
    */
   private void unionNeighbors(int row, int column, int position) {
 
@@ -114,27 +154,27 @@ public class Percolation {
     }
 
     // connects to top neighbor
-    if (row > 1 && (field[top])) {
+    if (row > 1 && field[top]) {
       quickUnionUF1.union(myIndex, top);
       quickUnionUF2.union(myIndex, top);
 
     }
 
     // connects to down neighbor
-    if (row < rowLength && (field[down])) {
+    if (row < rowLength && field[down]) {
       quickUnionUF1.union(myIndex, down);
       quickUnionUF2.union(myIndex, down);
 
     }
     // connects to right neighbor
-    if (column > 1 && (field[right])) {
+    if (column > 1 && field[right]) {
       quickUnionUF1.union(myIndex, right);
       quickUnionUF2.union(myIndex, right);
 
     }
 
     // connects to left neighbor
-    if (column < rowLength && (field[left])) {
+    if (column < rowLength && field[left]) {
       quickUnionUF1.union(myIndex, left);
       quickUnionUF2.union(myIndex, left);
 
@@ -143,47 +183,32 @@ public class Percolation {
   }
 
   /**
-   * @param row
-   * @param column
-   * @return true if the field is open
+   * Validating integers.
+   * 
+   * @param row row value in the matrix
+   * @param column column value in the matrix
    */
-  public boolean isOpen(int row, int column) {
-    validate(row, column);
-    int pos = xyTo1D(row, column);
-    return field[pos];
+  private void validate(int row, int column) {
+
+    if (row < 1 || row > rowLength || column < 1 || column > rowLength) {
+      throw new java.lang.IndexOutOfBoundsException(
+          "The values must be between 1 and " + rowLength);
+    }
+
 
   }
 
-  /**
-   * @param row
-   * @param column
-   * @return true if the site is full
-   */
-  public boolean isFull(int row, int column) {
-    validate(row, column);
-    if (isOpen(row, column)) {
-
-      int index = xyTo1D(row, column);
-      if (quickUnionUF2.connected(index, topRoot)) {
-
-        return true;
-      }
-      return false;
-    }
-    return false;
-
-  }
 
   /**
-   * @return true if the system percolates
+   * @param row row value in the matrix
+   * @param column column value in the matrix
+   * @return 1D coordinate value of the given field's 2D grid coordinate.
    */
-  public boolean percolates() {
-    if (quickUnionUF1.connected(topRoot, bottomRoot)) {
-      return true;
-    } else {
-      return false;
-    }
+  private int xyTo1D(int row, int column) {
 
+    row = row - 1;
+    column = column - 1;
+    return row * rowLength + column;
   }
 
 }
